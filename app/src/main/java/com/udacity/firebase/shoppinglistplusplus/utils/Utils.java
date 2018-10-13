ff70cd5ea629a24a75699197d26bb428e222de94
Michael Hofmann <blackukulele@web.de>
2018-10-13 21:23:34 +0200

    0.1 Add Locale to SimpleDateFormat

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/utils/Utils.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/utils/Utils.java
@@ -3,6 +3,7 @@ package com.udacity.firebase.shoppinglistplusplus.utils;
 import android.content.Context;
 
 import java.text.SimpleDateFormat;
+import java.util.Locale;
 
 /**
  * Utility class
@@ -11,7 +12,7 @@ public class Utils {
     /**
      * Format the date with SimpleDateFormat
      */
-    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+    public static final SimpleDateFormat SIMPLE_DATE_FORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm", Locale.GERMANY);
     private Context mContext = null;
 
 
