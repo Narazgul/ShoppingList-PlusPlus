151fe22b919a0e1270a41c44d06a0d69f257f1d7
Michael Hofmann <blackukulele@web.de>
2018-10-13 18:32:01 +0200

    0.1 Add ShoppingList Pojo

---
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/model/ShoppingList.java
@@ -0,0 +1,24 @@
+package com.udacity.firebase.shoppinglistplusplus.model;
+
+public class ShoppingList {
+    private String listName;
+    private String owner;
+
+    public ShoppingList() {
+    }
+
+    public ShoppingList(String listName, String owner) {
+        this.listName = listName;
+        this.owner = owner;
+    }
+
+    public String getListName() {
+        return listName;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+
+}
