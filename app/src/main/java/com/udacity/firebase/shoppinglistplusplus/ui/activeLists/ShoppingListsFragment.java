6086c2e05144d42cbcb5de36f2e8319f6ae13a84
Michael Hofmann <blackukulele@web.de>
2018-10-13 18:50:34 +0200

    0.1 Read and display single list from database

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeLists/ShoppingListsFragment.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeLists/ShoppingListsFragment.java
@@ -3,13 +3,25 @@ package com.udacity.firebase.shoppinglistplusplus.ui.activeLists;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.udacity.firebase.shoppinglistplusplus.R;
+import com.udacity.firebase.shoppinglistplusplus.model.ShoppingList;
+
+import javax.annotation.Nullable;
+
+import static com.udacity.firebase.shoppinglistplusplus.utils.Constants.ACTIVE_LISTS;
+import static com.udacity.firebase.shoppinglistplusplus.utils.Constants.LISTNAME;
 
 
 /**
@@ -18,7 +30,11 @@ import com.udacity.firebase.shoppinglistplusplus.R;
  * create an instance of this fragment.
  */
 public class ShoppingListsFragment extends Fragment {
+    public static final String TAG = ShoppingListsFragment.class.getSimpleName();
+
     private ListView mListView;
+    private TextView mTextViewListName;
+    private TextView mTextViewCreatedBy;
 
     public ShoppingListsFragment() {
         /* Required empty public constructor */
@@ -60,6 +76,30 @@ public class ShoppingListsFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_shopping_lists, container, false);
         initializeScreen(rootView);
 
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection(ACTIVE_LISTS).document(LISTNAME).addSnapshotListener(new EventListener<DocumentSnapshot>() {
+            @Override
+            public void onEvent(@Nullable DocumentSnapshot snapshot, @Nullable FirebaseFirestoreException e) {
+                if (e != null) {
+                    Log.w(TAG, "Listen failed.", e);
+                    return;
+                }
+
+                if (snapshot != null && snapshot.exists()) {
+                    Log.d(TAG, "Current data: " + snapshot.getData());
+
+                    ShoppingList list = snapshot.toObject(ShoppingList.class);
+                    if (list != null) {
+                        mTextViewListName.setText(list.getListName());
+                        mTextViewCreatedBy.setText(list.getOwner());
+                    }
+                } else {
+                    Log.d(TAG, "Current data: null");
+                }
+            }
+        });
+
         /**
          * Set interactive bits, such as click events and adapters
          */
@@ -83,6 +123,8 @@ public class ShoppingListsFragment extends Fragment {
      * Link layout elements from XML
      */
     private void initializeScreen(View rootView) {
-        mListView = (ListView) rootView.findViewById(R.id.list_view_active_lists);
+        mListView = rootView.findViewById(R.id.list_view_active_lists);
+        mTextViewListName = rootView.findViewById(R.id.text_view_list_name);
+        mTextViewCreatedBy = rootView.findViewById(R.id.text_view_created_by_user);
     }
 }
