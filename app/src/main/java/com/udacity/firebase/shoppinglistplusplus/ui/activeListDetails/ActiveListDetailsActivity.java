c7526944c60728199e4dd38b7a249846afc35817
Michael Hofmann <blackukulele@web.de>
2018-10-15 14:29:47 +0200

    0.2 Rename activity list detail

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeListDetails/ActiveListDetailsActivity.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/activeListDetails/ActiveListDetailsActivity.java
@@ -31,7 +31,7 @@ public class ActiveListDetailsActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_active_list_details2);
+        setContentView(R.layout.activity_active_list_details);
 
         initializeScreen();
     }
