51109a0b973a97fd5fe93064e8565eeead605584
Michael Hofmann <blackukulele@web.de>
2018-10-16 18:02:37 +0200

    0.3 Implement login

--- app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/login/LoginActivity.java
+++ app/src/main/java/com/udacity/firebase/shoppinglistplusplus/ui/login/LoginActivity.java
@@ -3,6 +3,8 @@ package com.udacity.firebase.shoppinglistplusplus.ui.login;
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
@@ -12,15 +14,17 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.udacity.firebase.shoppinglistplusplus.R;
-import com.udacity.firebase.shoppinglistplusplus.ui.BaseActivity;
+import com.udacity.firebase.shoppinglistplusplus.ui.MainActivity;
 
-public class LoginActivity extends BaseActivity {
-    private static final String LOG_TAG = LoginActivity.class.getSimpleName();
+public class LoginActivity extends BaseLoginActivity {
+    private static final String TAG = LoginActivity.class.getSimpleName();
 
-    /* A dialog that is presented until the Firebase authentication finished. */
     private ProgressDialog mAuthProgressDialog;
-    private EditText mEditTextEmailInput, mEditTextPasswordInput;
+    private EditText email, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +33,7 @@ public class LoginActivity extends BaseActivity {
 
         initializeScreen();
 
-        mEditTextPasswordInput.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        password.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
             public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
                 if (actionId == EditorInfo.IME_ACTION_DONE || keyEvent.getAction() == KeyEvent.ACTION_DOWN) {
@@ -40,13 +44,14 @@ public class LoginActivity extends BaseActivity {
         });
     }
 
-
     public void initializeScreen() {
-        mEditTextEmailInput = findViewById(R.id.edit_text_email);
-        mEditTextPasswordInput = findViewById(R.id.edit_text_password);
+
+        email = findViewById(R.id.edit_text_email);
+        password = findViewById(R.id.edit_text_password);
+
         LinearLayout linearLayoutLoginActivity = findViewById(R.id.linear_layout_login_activity);
         initializeBackground(linearLayoutLoginActivity);
-        /* Setup the progress dialog that is displayed later when authenticating with Firebase */
+
         mAuthProgressDialog = new ProgressDialog(this);
         mAuthProgressDialog.setTitle(getString(R.string.progress_dialog_loading));
         mAuthProgressDialog.setMessage(getString(R.string.progress_dialog_authenticating_with_firebase));
@@ -60,7 +65,20 @@ public class LoginActivity extends BaseActivity {
 
 
     public void onSignInPressed(View view) {
-        Toast.makeText(this, "SignIn pressed", Toast.LENGTH_SHORT).show();
+        auth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString())
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                            finish();
+                        } else {
+                            Log.d(TAG, task.getException().toString());
+                            Toast.makeText(LoginActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
 
@@ -68,5 +86,4 @@ public class LoginActivity extends BaseActivity {
         Intent intent = new Intent(LoginActivity.this, CreateAccountActivity.class);
         startActivity(intent);
     }
-
 }
